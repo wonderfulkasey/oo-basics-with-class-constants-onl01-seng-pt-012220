27ea5816e2bbcfe05fa35e2dcaf654359c99b15a
Kasey Arnold <kaseyarnold15@gmail.com>
2020-02-10 02:26:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,6 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
-  attr_reader :genre
+  attr_reader :title, :genre
   
   GENRES = []
 
@@ -13,7 +12,7 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
   
-  def initialize(genre)
+  def genre=(genre)
     @genre = genre
     GENRES << genre
   end
